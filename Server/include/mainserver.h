05915353afee59d8124fa3ebde27ccbb06553e5a
jcgod413 <jcgod413@gmail.com>
2017-05-16 23:19:49 +0900

    userInfo, gameRoom, sharedMemory struct added

--- Server/include/mainserver.h
+++ Server/include/mainserver.h
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <iostream>
 #include <queue>
+#include <list>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,25 +20,29 @@
 
 #define PORT 20001
 
-typedef struct game_room {
-	struct page_num *next;
-	struct page_num *prev;
-	int player1;
-	int player2;
-	int player3;
-	int player4;
-	int status;
-	pthread_t room_id;
-	//queue
-	char name[30];
-} game_room;
+using namespace std;
 
-struct game_room *play_list = NULL;
-struct game_room *wait_list = NULL;
+struct userInfo	{
+	int number;		// user index
+	int FD;			// file descriptor
+}
 
-typedef struct thread_param{
+struct game_room {
+	pthread_t roomID;	// room id(thread num)
+	int status;			// game status (wait, play)
+	int turn;			// current turn user index
+
+	list<userInfo> userList;		// user list
+	queue<Message> messageQueue;	// message queue (row message)
+};
+
+struct shared_memory	{
+	list<game_room> roomList;
+};
+
+struct thread_param{
 	int client_fd;
-}thread_param;
+};
 
 void createRoom(Message *);
 #endif
