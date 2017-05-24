a56f400e94ebe41053a03154ad903ec6434983d7
Sangwon Hong <qpakzk@gmail.com>
2017-05-24 16:19:32 +0900

    update client again

---
+++ Client/Assets/Scripts/Room_Info.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Room_Info : MonoBehaviour {
+    public int Room_ID;
+    public string Name;
+    public string Player_Num;
+    public int status;
+    public Button.ButtonClickedEvent OnButtonClick;
+}
---
+++ Client/Assets/Scripts/Room_Object.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Room_Object : MonoBehaviour {
+    public int Room_ID;
+    public Button Button;
+    public Text Name;
+    public Text Player_Num;
+}
