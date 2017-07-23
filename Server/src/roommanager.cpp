be8b87ab9993ede2a08fe385817f31f3122dab0a
Sangwon Hong <qpakzk@gmail.com>
2017-07-23 18:26:32 +0900

    indent

--- Server/src/roommanager.cpp
+++ Server/src/roommanager.cpp
@@ -186,7 +186,7 @@ void startRoom(Message *message, Message *response) {
   // game_room 상태 변경
   pthread_mutex_lock(&mutex_lock);
 
-	current_game->status = PLAY;
+  current_game->status = PLAY;
   current_game->turn = 1;
 
   for(list<userInfo>::iterator it2 = current_game->userList.begin(); it2 != current_game->userList.end(); ++it2) {
